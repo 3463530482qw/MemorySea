98553fee48c85d7ea10f0fde3332defd86c9a87b
3463530482qw <3463530482@qq.com>
2026-08-21 20:30:35 +0800

    0.0.071

--- sence/mainMenu/mainMenu.cpp
+++ sence/mainMenu/mainMenu.cpp
@@ -1,5 +1,14 @@
 #include "vmode.inl"
 
 extern "C" EXPORT void mainMenu() {
-    
+    int a{0};
+    线程.update_pack([&a](){
+        a++;
+    });
+    线程.draw_pack([&a](){
+        std::println("输出a的值是:{}", a);
+    });
+    while(1) {
+        线程.run();
+    }
 }
