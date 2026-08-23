e857c2df39db861805633dd208a2503b62d0752e
3463530482qw <3463530482@qq.com>
2026-08-24 02:47:36 +0800

    0.0.074

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ int main() {
             场景.目标场景 = 场景.当前场景;
             运行.指定场景(场景);
             运行.指定线程(线程);
+            Gnik_luos::窗口配置信息 窗口配置信息;
+            窗口配置信息.batch_load(配置文件.获取("初始化配置"),"window_info");
+            std::println("输出a的值是:{}", 配置文件.logic_height);
         }
         while(1) {
             if (auto it = 场景.数据.find(场景.目标场景); it != 场景.数据.end()) {
