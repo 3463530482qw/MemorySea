b9ae732986a395933c1c70d5056fa255c87dcffb
3463530482qw <3463530482@qq.com>
2026-08-27 23:53:00 +0800

    0.0.083

--- sence/mainMenu/mainMenu.cpp
+++ sence/mainMenu/mainMenu.cpp
@@ -1,12 +1,12 @@
 #include "vmode.inl"
 
 extern "C" EXPORT void mainMenu() {
-    int a{0};
+    int a{0};     
     线程.更新包([&a](){
         a++;
     });
     线程.绘制包([&a](){
-        std::println("输出a的值是:{}", 窗口.time.current);
+        std::println("第{}次输出值是:{}", 窗口.mouse.y, 窗口.mouse.x);
     });
     运行.开始();
 }
